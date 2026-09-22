3da864d9898859c15c8d91e953f00ab358e920dc
YaoSimlin <nguessan.yao23@inphb.ci>
2026-09-22 15:29:57 +0000

    Retrait scripts de generation de hash

--- hash.py
+++
@@ -1,8 +0,0 @@
-import streamlit_authenticator as stauth
-
-passwords = ["sig12026", "esg22026", "manager123"]
-
-hashed = stauth.Hasher.hash(passwords)
-
-for pwd, h in zip(passwords, hashed):
-    print(f"{pwd}  ->  {h}")
